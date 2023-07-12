dbeeaf997723ee72647f380f5b12e71b299724fe
Avillar0211 <99834973+Avillar0211@users.noreply.github.com>
2023-07-12 11:21:00 +0200

    Sin aplicacion actualizado y funcional

--- sin_aplicacion.py
+++ sin_aplicacion.py
@@ -3,6 +3,12 @@ import pandas
 import numpy
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import max_error
+from sklearn.metrics import mean_absolute_error
+
 from sklearn.preprocessing import LabelEncoder
 
 datos = []
@@ -32,38 +38,51 @@ for x in aux:
 aux = dataset.select_dtypes(include=numpy.number).columns.tolist()
 for x in aux:
     mitjana = dataset[x].mean()
-    dataset[x].fillna(mitjana, inplace=True)  
+    dataset[x].fillna(mitjana, inplace=True) 
+label_encoder = LabelEncoder()
+aux = dataset.select_dtypes(exclude=numpy.number).columns.tolist()
+for x in aux:
+    if dataset[x].nunique() < 5:
+        label_encoder = LabelEncoder()
+        dataset[x] = label_encoder.fit_transform(dataset[x])
 
 
 dataset= dataset.sort_values(by = ["CODI COMARCA", "DATA"])
+#dataset.reset_index(drop=True)
 dataset['MAXIM'] = dataset.iloc[:, 12:35].max(axis=1)
+dataset['MINIM'] = dataset.iloc[:, 12:35].min(axis=1)
 dataset['MITJANA_DIA'] = dataset.iloc[:, 12:35].mean(axis=1)
 dataset['DIA']=pandas.to_datetime(dataset['DATA'],format='%d/%m/%Y').dt.day
 dataset['MES']=pandas.to_datetime(dataset['DATA'],format='%d/%m/%Y').dt.month
 dataset['ANY']=pandas.to_datetime(dataset['DATA'],format='%d/%m/%Y').dt.year
 dataset.drop(['DATA'],axis=1,inplace=True)
-label_encoder = LabelEncoder()
-columnas_string= dataset.select_dtypes(exclude=[numpy.number]).columns
-for i in columnas_string:
-    dataset[i] = label_encoder.fit_transform(dataset[i])
+dataset.drop('Unnamed: 0', inplace = True, axis = 1)
+dataset.round()
 
-print("Label Encoded Data: ")
-
-
-print(dataset.head())
-
-features = numpy.array(dataset)
-labels = numpy.array(dataset['MAXIM'])
+aux = dataset.select_dtypes(include=[numpy.number])
+features = numpy.array(aux.iloc[:, :-1])
+labels = numpy.array(aux.iloc[:, -1])
 train_x, test_x, train_y, test_y = train_test_split(features, labels, test_size = 0.2)
 
-rf = RandomForestRegressor(10, random_state=0)
+rf = RandomForestRegressor(n_estimators=100, random_state=0)
+
 rf.fit(train_x, train_y)
 
-test_y.reshape(1, -1)
-print(rf.score(test_y, test_x))
-
 pred = rf.predict(test_x)
-assessment = accuracy_score(test_y, pred)
+
+accuracy_score = accuracy_score(test_y, pred)
+balanced_accuracy_score = balanced_accuracy_score(test_y, pred)
+average_precision_score = average_precision_score(test_y, pred)
+precision_score = precision_score(test_y, pred)
+max_error = max_error(test_y, pred)
+mean_absolute_error = mean_absolute_error(test_y, pred)
+
+print(accuracy_score)
+print(balanced_accuracy_score)
+print(average_precision_score)
+print(precision_score)
+print(max_error)
+print(mean_absolute_error)
 
 print("ok")
 
