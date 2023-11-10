3db2691b40b557c3bd5000a0df6df9fcf9e4fc0a
Avillar0211 <99834973+Avillar0211@users.noreply.github.com>
2023-11-10 09:56:45 +0100

    Add files via upload

--- Data_Transformation/Data_Transform.py
+++ Data_Transformation/Data_Transform.py
@@ -5,26 +5,40 @@ from Data_Transformation.Transformaciones.filtrar_por_un_valor import Filtrar_po
 from Data_Transformation.Transformaciones.ordenar_columnas import Ordenar_columnas
 from Data_Transformation.Transformaciones.dividir_dummies import Dividir_dummies
 from Data_Transformation.Transformaciones.separar_fecha import Separar_fecha
+from Data_Transformation.Transformaciones.separar_colesterol import Separar_colesterol
+from Data_Transformation.Transformaciones.agregar_LDL_discreto import Agregar_LDL_discreto
+from Data_Transformation.Transformaciones.agregar_total_discreto import Agregar_total_discreto
 
 class DataTransform():
     def __init__(self, dFrame, Transformaciones):
         self.singleDataTransformator = []
         self.dFrame = dFrame
         for proc in Transformaciones:
-            formatClassInstance = globals()[proc]
-            self.singleDataTransformator.append((formatClassInstance))
+            if isinstance(proc, list):
+                formatClassInstance = globals()[proc[0]](proc[1])
+                self.singleDataTransformator.append((formatClassInstance))
+            else: 
+                formatClassInstance = globals()[proc]
+                self.singleDataTransformator.append(formatClassInstance)
         return
     
     def load_transformations(self, dFrame, Transformaciones):
         self.dFrame = dFrame
         for proc in Transformaciones:
-            formatClassInstance = globals()[proc]
-            self.singleDataTransformator.append((formatClassInstance))
+            if isinstance(proc, list):
+                formatClassInstance = globals()[proc[0]]
+                self.singleDataTransformator.append([formatClassInstance, proc[1]])
+            else: 
+                formatClassInstance = globals()[proc]
+                self.singleDataTransformator.append(formatClassInstance)
         return
     
     def data_transform(self):
         for i in self.singleDataTransformator:
-            self.dFrame = i.transform(self, self.dFrame)
+            if isinstance(i, list):
+                self.dFrame = i[0].transform(self, self.dFrame, i[1])
+            else:
+                self.dFrame = i.transform(self, self.dFrame)
         return self.dFrame
       
         
