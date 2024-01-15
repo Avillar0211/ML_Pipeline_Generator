e94fd4fce1f57b7851ebaffd5a3c308e5f805ffd
Avillar0211 <99834973+Avillar0211@users.noreply.github.com>
2024-01-15 08:26:52 +0100

    Añado librería
    Mismo contenido pero ahora viene en formato librería para mostrar como se vera al final

---
+++ Data_Collection/Formatos/CSV_DataCollector.py
@@ -0,0 +1,14 @@
+from ML_Pipeline_Generator.Data_Collection.Formatos.Single_DataCollector import SingleDataCollector
+import pandas
+
+class CSVDataCollector(SingleDataCollector):
+
+    def __init__(self):
+        super().__init__()
+
+    def read(self, Filename):
+        aux = pandas.read_csv(Filename)
+        Frame =  pandas.DataFrame(aux)
+        return Frame
+    
+    
